525741ab273581b97055b470c4419e1b9b787afc
LucasCarmusciano <lucascarmusciano@gmail.com>
2022-09-26 21:13:58 -0300

    Creo el constructor

---
+++ tp5/tp5_2/Alumno.java
@@ -0,0 +1,13 @@
+package tp5.tp5_2;
+
+import java.util.ArrayList;
+
+public class Alumno {
+    private String nombre;
+    
+    public Alumno(String nombre){
+        this.nombre = nombre;
+        ArrayList<String> cualidades = new ArrayList<>();
+        ArrayList<Alumno> familiares = new ArrayList<>();
+    }
+}
---
+++ tp5/tp5_2/Casa.java
@@ -0,0 +1,16 @@
+package tp5.tp5_2;
+
+import java.util.ArrayList;
+
+public class Casa {
+    private String nombre;
+    private int cantMaxAlumnos;
+
+    public Casa(String nombre, int cantMaxAlumnos){
+        this.nombre = nombre;
+        Alumno[] listaAlumnos = new Alumno[cantMaxAlumnos];
+        ArrayList<String> cualidadesNecesarias = new ArrayList<>();
+    }
+
+    
+}
